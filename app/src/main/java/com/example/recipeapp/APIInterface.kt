7513d785237d5b55d3765ebe56e1fdfa8b6c9b0f
rayanaOmar <rayanaomar_@hotmail.com>
2021-10-12 19:16:02 +0300

    RecipeApp

---
+++ app/src/main/java/com/example/recipeapp/APIInterface.kt
@@ -0,0 +1,18 @@
+package com.example.recipeapp
+
+import retrofit2.Call
+import retrofit2.http.Body
+import retrofit2.http.GET
+import retrofit2.http.Headers
+import retrofit2.http.POST
+
+interface APIInterface {
+    @Headers("Content-Type: application/json")
+    @GET("/recipes/")
+    fun getRecipies(): Call<List<RecipeDetails.Datum>>
+
+
+    @Headers("Content-Type: application/json")
+    @POST("/recipes/")
+    fun addRecipie(@Body userData: RecipeDetails.Datum): Call<RecipeDetails>
+}
